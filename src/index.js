c125df9b5646f98bd1d2452710005bc04043dde9
HakasLepehen <doctorrosen2016@gmail.com>
2021-01-18 17:54:11 +0600

    new commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import Post from './post.js';
 let post = new Post('Webpack');
 
 console.log(JSON.stringify(post));
+console.log('Hi pidor!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
