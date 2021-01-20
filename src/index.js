2bab3636004deb7fe43cf6df29c870244f4f62f3
HakasLepehen <doctorrosen2016@gmail.com>
2021-01-20 17:51:19 +0600

    another commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import Post from './post.js';
 import * as $ from 'jquery';
 import './styles/style.css'
+import './styles/less.less'
 
 const post = new Post('Webpack');
 
 console.log(JSON.stringify(post));
 $('pre')
-.html(JSON.stringify(post) + '1')
+.html(JSON.stringify(post))
 .addClass('vendor');
 
 console.log('Hi');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,32 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const optimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+                chunks: 'all'
+        }
+    }
+
+    if(isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+}
+
+
+console.log('IsDev: ', isDev);
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -9,29 +35,56 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     devServer: {
         port: 4200
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css'
+            })
     ],
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.css$/, //css-загрузчик
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: 
+                        {
+                            publicPath: ''
+                        }
+                    }, 
+                    {
+                        loader: 'css-loader'
+                }
+                ]
             },
             {
                 test: /\.(ttf|woff|woff2|eot)$/,
                 use: ['file-loader']
+            },
+            {
+                test: /\.less$/, //css-загрузчик
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: 
+                        {
+                            publicPath: ''
+                        }
+                    }, 
+                    'css-loader',
+                    'less-loader'
+                ]
             }
         ]
     }
